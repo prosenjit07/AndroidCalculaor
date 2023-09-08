a8b0c6ef37318b3161a6111e14ccf1fa2ba83ef9
Prosenjit <prosenjitbiswas983@gmail.com>
2023-09-08 11:26:24 +0600

    and or xor button add

--- app/src/main/java/com/android/prosenjit/MainActivity.java
+++ app/src/main/java/com/android/prosenjit/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.lang.Math;
 
 public class MainActivity extends AppCompatActivity {
     EditText fNumEditText, sNumEditText;
@@ -21,23 +22,38 @@ public class MainActivity extends AppCompatActivity {
         outputTextView = findViewById(R.id.output);
     }
     public void myButton(View view) {
-        double fNum = Double.parseDouble(fNumEditText.getText().toString());
-        double sNum = Double.parseDouble(sNumEditText.getText().toString());
-        double result=0;
-        if(view.getId()==(R.id.sumButtonID)){
-            result= fNum+sNum;
+        try {
+            double fNum = Integer.parseInt(fNumEditText.getText().toString());
+            double sNum = Integer.parseInt(sNumEditText.getText().toString());
+            double result = 0;
+
+            if (view.getId() == (R.id.sumButtonID)) {
+                result = fNum + sNum;
+            } else if (view.getId() == (R.id.subButtonID)) {
+                result = fNum - sNum;
+            } else if (view.getId() == (R.id.mulButtonID)) {
+                result = fNum * sNum;
+            } else if (view.getId() == (R.id.divButtonID)) {
+                if (sNum != 0) {
+                    result = fNum / sNum;
+                } else {
+                    Toast.makeText(this, "Division by zero is not allowed.", Toast.LENGTH_SHORT).show();
+                    return; // Exit the method
+                }
+            } else if (view.getId() == (R.id.modButtonID)) {
+                    result = fNum % sNum;
+            } else if (view.getId() == (R.id.xorButtonID))  {
+              //result = (int) Math.round(fNum) ^ (int) Math.round(sNum);
+                result = (int)(fNum) ^ (int)sNum;
+            } else if (view.getId() == (R.id.andButtonID)) {
+                result = (int)(fNum) & (int)sNum;
+            } else if (view.getId() == (R.id.orButtonID)) {
+                result = (int)(fNum) | (int)sNum;
+            }
+            String mal = String.format("%d", (int) result);
+            outputTextView.setText("Result is: " + mal);
+        } catch (Exception e) {
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
         }
-        else if(view.getId()==(R.id.subButtonID)){
-            result= fNum-sNum;
-        }
-        else if(view.getId()==(R.id.mulButtonID)){
-            result= fNum*sNum;
-        }
-        else if(view.getId()==(R.id.divButtonID)){
-            result= fNum/sNum;
-        }
-        String mal =String.format("%.2f",result);
-        outputTextView.setText("Result is: "+mal);
-        //Toast.makeText(this, "Output is: "+result, Toast.LENGTH_SHORT).show();
     }
 }
